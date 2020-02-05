1ae9f461f91b33d0f1058f04e194bfbf8c272e94
Daro Omwanor <osirisdaro@gmail.com>
2020-02-04 20:54:51 -0500

    Admin Panel

--- rawblumeApp/routes.py
+++ rawblumeApp/routes.py
@@ -4,5 +4,10 @@ from flask import render_template, request
 from rawblumeApp import app
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
+
+
+@app.route("/Admin_Panel")
+def Admin_Panel():
+    return render_template('Admin_Panel.html')
