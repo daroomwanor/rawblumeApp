e6e6c086faa7f7fd2bec775cdce94c556e4e57cb
Daro Omwanor <osirisdaro@gmail.com>
2020-02-05 11:17:10 -0500

    Update

--- rawblumeApp/__init__.py
+++ rawblumeApp/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4973490d9fdsf-0dr-340-348347850450'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/www/rawblumeApp/rawblumeApp.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rawblumeApp.db'
 
 db = SQLAlchemy(app)
 
